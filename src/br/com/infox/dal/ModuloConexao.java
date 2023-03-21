be52b9ab69c6ddaa35dd310e2bc21b2b5d59a1d9
Bruno-Özen <112650688+Bruno-Ozen@users.noreply.github.com>
2023-03-21 15:01:27 -0300

    Primeiro commit
    Programação inicial da interface de login e tela principal.

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.infox.dal;
+import java.sql.*;
+/**
+ *
+ * @author bruno
+ */
+public class ModuloConexao {
+    // Método responsável por estabelecer a conexão com o banco
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        // A linha abaixo chama o driver:
+        String driver = "com.mysql.cj.jdbc.Driver";
+        // Armazenando informações referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "";
+        // Estabelecendo a conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
