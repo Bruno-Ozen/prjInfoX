b980f8910a4b773a05d8638618ea1a83d373ef45
Bruno Henrique de Pinho <112650688+Bruno-Ozen@users.noreply.github.com>
2023-06-26 15:58:59 -0300

    Formatação UTF-8 da DB
    Formatação UTF-8 da DB, instalação do iReport e Java 7.

--- src/br/com/infox/dal/ModuloConexao.java
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -15,9 +15,9 @@ public class ModuloConexao {
         // A linha abaixo chama o driver:
         String driver = "com.mysql.cj.jdbc.Driver";
         // Armazenando informações referente ao banco
-        String url = "jdbc:mysql://localhost:3306/dbinfox";
-        String user = "root";
-        String password = "";
+        String url = "jdbc:mysql://localhost:3306/dbinfox?characterEncoding=utf-8";
+        String user = "dba";
+        String password = "infox@123456";
         // Estabelecendo a conexão com o banco
         try {
             Class.forName(driver);
